14fe53a98334efeec7dab7ccd9721ed71354c643
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-05-14 23:36:38 +0200

    filter v1

--- components/auxiliar.js
+++ components/auxiliar.js
@@ -160,6 +160,58 @@ export const getRestaurants = async (token) => {
 /**
  * Get restaurants
  * @param {string} token auth
+ * @param {object} values restaurant values
+ * @returns {object} Object with error or data properties
+ */
+export const filterRestaurants = async (token, values) => {
+  const url ="http://127.0.0.1:8080/FoodDelivery/rest/restaurants/filter";
+  try {
+    const formData = new URLSearchParams();
+    if(values.id){
+      formData.append("name", values.name);
+    }
+    if(values.name){
+      formData.append("address", values.address);
+    }
+    if(values.address){
+      formData.append("available", values.available);
+    }
+    if(values.telephone){
+      formData.append("bikeFriendly", values.bikeFriendly);
+    }
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: "Bearer " + token,
+      },
+      body: formData.toString(),
+    });
+    const data = await response.json();
+    if (data.error) {
+      console.log(data.error);
+      return {
+        error: data.error,
+        data: null,
+      };
+    } else {
+      return {
+        error: null,
+        data: data,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      error: error,
+      data: null,
+    };
+  }
+}
+
+/**
+ * Get restaurants
+ * @param {string} token auth
  * @param {float} id restaurant
  * @returns {object} Object with error or data properties
  */
